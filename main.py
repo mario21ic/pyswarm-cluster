53a0ca7791d528f19660217b7bb06bf9a90a60fb
Mario Inga <mario@yarkan.pe>
2018-02-23 18:20:00 -0500

    feat(global) adding logging

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env python3
-
-
+####    IMPORTS
 import os
+import boto3
 import logging
+import platform
 
 from subprocess import run
 from subprocess import call
 from subprocess import PIPE
 
-import boto3
-import platform
 import docker
 
-
-role = os.environ['ROLE']
-current_instance = os.environ['INSTANCE']
+logging.basicConfig(filename="/tmp/output_swarm.log", level=logging.INFO, format="%(asctime)s " + platform.node() + ": %(message)s")
+docker_client = docker.from_env()
 
 ec2 = boto3.client('ec2')
-docker_client = docker.from_env()
-docker_api = docker.APIClient(base_url='unix://var/run/docker.sock')
-logging.basicConfig(level=logging.INFO, format="%(asctime)s " + platform.node() + ": %(message)s")
-
+role = os.environ['ROLE']
+current_instance = os.environ['INSTANCE']
+hostname = platform.node()
 
 def add_tag(tags):
     return ec2.create_tags(Resources=[current_instance], Tags=tags)
-
 
 def count_nodes(nodes):
     count = 0
@@ -42,18 +37,17 @@ managers_running = ec2.describe_instances(Filters=[
     {'Name': 'tag:Init', 'Values': ['true']}])
 
 instances_count, instances = count_nodes(managers_running['Reservations'])
-hostname = platform.node()
+
 
 if instances_count > 0 or role == 'worker':
     manager = instances[0]['PrivateIpAddress']
-    call(["echo", "'Here I join to the cluster {}'".format(manager)])
-
+    logging.info("'Here I join to the cluster {}'".format(manager))
     token = run(["docker", "-H {}:2376".format(manager), "swarm", "join-token", "-q", role], stdout=PIPE)
     call(['docker', 'swarm', 'join', '{}:2377'.format(manager), '--token', token.stdout.decode('utf-8').replace('\n', '')])
 
     add_tag([{'Key': 'Hostname', 'Value': hostname}])
-    # if role == 'manager':
-    #     add_tag([{'Key': 'Init', 'Value': 'true'}])
+    if role == 'manager':
+        add_tag([{'Key': 'Init', 'Value': 'true'}])
 
 else:
     try:
@@ -62,4 +56,7 @@ else:
     except Exception as e:
         logging.error("Swarm Init Error: " + str(e))
 
-    add_tag([{'Key': 'Init', 'Value': 'true'}, {'Key': 'Hostname', 'Value': hostname}])
+    hostname = run(['hostname'], stdout=PIPE)
+    add_tag([
+        {'Key': 'Init', 'Value': 'true'},
+        {'Key': 'Hostname', 'Value': hostname}])
